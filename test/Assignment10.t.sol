ce66c9527928fb540a59f5e2dca48e324fef5ac3
ahmedmo2399 <ahmedabdelmoeen99@gmail.com>
2025-03-23 21:04:45 +0200

    Assignment

--- test/Assignment10.t.sol
+++ test/Assignment10.t.sol
@@ -2,10 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {Test} from "forge-std/Test.sol";
+import {Assignment10} from "../src/Assignment10.sol";
 
 contract Assignment10Test is Test {
-    function testAlwaysPass() public {
-        // This is a gift assignment :)
-        assertTrue(true);
+    Assignment10 assignment;
+
+    function setUp() public {
+        assignment = new Assignment10();
+    }
+
+    function testFeeIsConstant() public {
+        assertEq(assignment.FEE(), 100, "FEE should be 100");
+    }
+
+    function testSetValue() public {
+        assignment.setValue(42);
     }
 }
